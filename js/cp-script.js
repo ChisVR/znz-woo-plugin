65f655efd139678f09219f70de317d4024b47b04
ChisVR <chisdeal2014@hotmail.com>
2022-12-07 21:17:55 +0000

    eep

--- js/cp-script.js
+++ js/cp-script.js
@@ -61,7 +61,7 @@ jQuery.noConflict();
                 url: '/wp-admin/admin-ajax.php',
                 type: "POST",
                 data: {
-                    action: "scc_verify_payment",
+                    action: "ZNZ_verify_payment",
                     order_id: $('input[name="cp_order_id"]').val()
                 },
                 dataType: "json",
